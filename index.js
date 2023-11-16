671fe9be11c947e83ff69a8c78cbfe0ec255b38f
ahmadharthey <ahmoood@outlook.com>
2023-11-16 22:40:17 +0300

    updated intents and fixed an issue with the token

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require('dotenv').config()
 const discord=require('discord.js')
 const client=new discord.Client({
-    intents:['GuildMembers',
-    'Guilds',
-    'GuildInvites',
+    intents:['GuildInvites',
 ]
 })
-client.login({token:process.env.LOGINTOKEN})
+client.login(process.env.LOGINTOKEN)
 client.on('guildMemberAdd',(member)=>{
     member.roles.add('1160843235112001568')
 })
