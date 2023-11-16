d2102efa6977ae9e232e8b4a1ef7e2c7cc9995fa
ahmadharthey <ahmoood@outlook.com>
2023-11-16 23:02:53 +0300

    bug fixes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
+const express=require('express')
+const app=express()
 const discord=require('discord.js')
 const client=new discord.Client({
-    intents:['GuildInvites',
+    intents:['GuildMembers',
 ]
 })
 client.login(process.env.LOGINTOKEN)
 client.on('guildMemberAdd',(member)=>{
-    member.roles.add('1160843235112001568')
+    member.roles.add('1160843235112001568').then(()=>{console.log('new member!')}).catch((err)=>{console.log(err)})
+    console.log('invite!')
+})
+app.listen(8080)
+app.route('/').get((req,res)=>{
+    res.send('bot working!')
 })
